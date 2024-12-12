adfa901d4d98bbf57603c805e55a13537cf8944f
Paul Wilson <wilson@imfusion.com>
2024-12-12 11:15:41 +0100

    rename package

---
+++ prostNfound/externals/medsam/__init__.py
@@ -0,0 +1,3 @@
+"""
+All code here taken from https://github.com/bowang-lab/MedSAM under open source license specified in the LICENSE file in this directory.
+"""
---
+++ prostNfound/externals/sam_med2d/__init__.py
@@ -0,0 +1,3 @@
+"""
+All code here taken from https://github.com/OpenGVLab/SAM-Med2D under open source license specified in the LICENSE file in this directory.
+"""
