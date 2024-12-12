c54216abc1950169be44ce804460c89cc5d6b77c
Paul Wilson <wilson@imfusion.com>
2024-12-12 11:16:08 +0100

    rename packages

--- vendor/medsam/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-All code here taken from https://github.com/bowang-lab/MedSAM under open source license specified in the LICENSE file in this directory.
-"""
--- vendor/sam_med2d/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-All code here taken from https://github.com/OpenGVLab/SAM-Med2D under open source license specified in the LICENSE file in this directory.
-"""
